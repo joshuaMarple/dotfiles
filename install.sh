9164f6228dd3b8b3a89ef5e852a91fad928344c0
marpljos <marpljos@penguin>
2020-08-23 22:39:58 -0700

    change install for plugged from vim to neovim

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ while true; do
   esac
 done
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ln -s ~/dotfiles/aliases ~/.bash_aliases
 ln -s ~/dotfiles/zshrc ~/.zshrc
