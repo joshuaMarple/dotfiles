5c54df7309fda8ae99f17688e6719eb0959dba3b
marpljos <marpljos@penguin>
2020-08-23 21:12:23 -0700

    flipping update and upgrade to be in the right order

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
-sudo -S apt-get upgrade
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo -S apt-get update
+sudo -S apt-get upgrade
 sudo -S apt-get -y install \
   neovim \
   ranger \
