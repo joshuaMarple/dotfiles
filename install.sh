0635ae1b5c3f6ae50414256d6f5c6f2c781e8a6b
Joshua Marple <joshua.d.marple@gmail.com>
2021-11-10 20:14:52 -0800

    reorganizing dotfiles

--- install.sh
+++ install.sh
@@ -25,9 +25,9 @@ sudo -S apt-get autoremove
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-ln -s ~/dotfiles/aliases ~/.bash_aliases
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/zsh/aliases ~/.bash_aliases
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 mkdir ~/.config
 mkdir ~/.config/nvim
 ln -s ~/dotfiles/nvimrc ~/.config/nvim/init.vim
