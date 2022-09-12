e9f46c648e42bc59092f8ef449fe61520e230260
Joshua Marple <joshua.d.marple@gmail.com>
2022-09-12 16:58:10 -0700

    cleaning up some inconsistent punctuation

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -2,6 +2,6 @@ require('config.nvim-lsp')
 require('config.web-devicons')
 require('config.telescope')
 require('config.treesitter')
-require'dotline'
+require('dotline')
 require('config.mini')
 require('leap').set_default_keymaps()
